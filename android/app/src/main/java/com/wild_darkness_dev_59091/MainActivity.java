72db45fa5321965814f17835b3dc55bd8c393d03
Crowdbotics <engineering@crowdbotics.com>
2023-01-24 04:39:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/wild_darkness_dev_59091/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wild_darkness_dev_59091;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "wild_darkness_dev_59091";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-darkness-dev-59091",
+  url: "https://wild-darkness-dev-59091.botics.co",
+  api: "https://wild-darkness-dev-59091.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
